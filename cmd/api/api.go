5619c71f1e6c3f06959f1eb1af9acf3823d7ff7c
minhnghia2k3 <brozennvn@gmail.com>
2024-10-03 16:26:45 +0700

    feat: add extra UI for confirmation

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/minhnghia2k3/GOssage/docs"
 	_ "github.com/minhnghia2k3/GOssage/docs"
 	"github.com/minhnghia2k3/GOssage/internal"
@@ -54,6 +55,15 @@ const version = "0.0.1"
 
 func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	// Middleware stack
 	r.Use(middleware.RequestID)
---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/v1"
+function App() {
+    return (
+        <>
+            <div>App home screen</div>
+        </>
+    )
+}
+export default App
